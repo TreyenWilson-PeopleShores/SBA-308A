a802601a39b018cdc95f22ad13f1fb0940bd0478
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-13 15:41:28 -0500

    Initial Structure

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const API_KEY =
+  "live_VOZbbd602q8H85w6A0laOJiAuJk7unXGEE8KYq3i9HNimRuFc7xzkIxA5sAOE71";
+
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+axios.defaults.baseURL = "https://api.thecatapi.com/v1";
+
 
